199905ef5cf6154098fd833118f297667edb3053
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2025-02-22 02:27:59 +0545

    Fix: Eslint error

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,29 +1,26 @@
-import { extractIdFromSlug } from "@/utils";
-
-import Await from "@/components/Await";
-import { BlogDetails } from "@/components/blog";
-import { BlogDetailsLoader } from "@/components/ui/loaders";
-import { Suspense } from "react";
-import { v4 as uuid } from "uuid";
+import Await from '@/components/Await';
+import { BlogDetails } from '@/components/blog';
+import { BlogDetailsLoader } from '@/components/ui/loaders';
+import { Suspense } from 'react';
+import { v4 as uuid } from 'uuid';
 
 async function fetchBlogById(slug: string) {
   try {
-
     const res = await fetch(`${process.env.NEXT_API_BASE_URL}/blogs/${slug}`);
 
     if (!res.ok) {
-      throw new Error("Unexpected Error occurred. Please try again later.");
+      throw new Error('Unexpected Error occurred. Please try again later.');
     }
 
     const resData = await res.json();
 
     if (!resData.data) {
-      throw new Error("Unexpected Error occurred. Please try again later.");
+      throw new Error('Unexpected Error occurred. Please try again later.');
     }
 
     return resData.data;
   } catch (error) {
-    console.error("Error fetching blog:", error);
+    console.error('Error fetching blog:', error);
     return null;
   }
 }
@@ -35,12 +32,12 @@ export default async function BlogDetailsPage({
 }) {
   const slug = (await params).slug;
 
-  const blog = fetchBlogById(slug?.toString() || "");
+  const blog = fetchBlogById(slug?.toString() || '');
 
   return (
     <div key={uuid()}>
       <Suspense fallback={<BlogDetailsLoader />}>
-        <Await promise={blog}>{(data) => <BlogDetails blog={data} />}</Await>
+        <Await promise={blog}>{data => <BlogDetails blog={data} />}</Await>
       </Suspense>
     </div>
   );
